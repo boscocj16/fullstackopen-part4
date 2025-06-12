373c8838e02b1bfccc602822b0d4735202bf163b
boscocj16 <boscojoseph79@gmail.com>
2025-06-12 23:43:14 +0530

    4.5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -2,59 +2,47 @@ const { describe, test } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-describe('total likes', () => {
-  const emptyList = []
+const listWithMultipleBlogs = [
+  {
+    _id: '1',
+    title: 'First Blog',
+    author: 'Author A',
+    url: 'http://a.com',
+    likes: 3,
+    __v: 0,
+  },
+  {
+    _id: '2',
+    title: 'Second Blog',
+    author: 'Author B',
+    url: 'http://b.com',
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: '3',
+    title: 'Third Blog',
+    author: 'Author C',
+    url: 'http://c.com',
+    likes: 5,
+    __v: 0,
+  },
+]
 
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  const listWithMultipleBlogs = [
-    {
-      _id: '1',
-      title: 'First Blog',
-      author: 'Author A',
-      url: 'http://a.com',
-      likes: 3,
-      __v: 0
-    },
-    {
-      _id: '2',
-      title: 'Second Blog',
-      author: 'Author B',
-      url: 'http://b.com',
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: '3',
-      title: 'Third Blog',
-      author: 'Author C',
-      url: 'http://c.com',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  test('of empty list is zero', () => {
-    const result = listHelper.totalLikes(emptyList)
-    assert.strictEqual(result, 0)
+describe('favorite blog', () => {
+  test('returns null for empty list', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.strictEqual(result, null)
   })
 
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    assert.strictEqual(result, 5)
+  test('returns the only blog when one blog is present', () => {
+    const blog = listWithMultipleBlogs[1]
+    const result = listHelper.favoriteBlog([blog])
+    assert.deepStrictEqual(result, blog)
   })
 
-  test('of a bigger list is calculated right', () => {
-    const result = listHelper.totalLikes(listWithMultipleBlogs)
-    assert.strictEqual(result, 15)
+  test('returns the blog with most likes', () => {
+    const result = listHelper.favoriteBlog(listWithMultipleBlogs)
+    assert.deepStrictEqual(result, listWithMultipleBlogs[1]) // Second Blog with 7 likes
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,12 +1,17 @@
-const dummy = (blogs) => {
-  return 1
-}
+const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return blogs.reduce((fav, blog) => (blog.likes > fav.likes ? blog : fav), blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 }
