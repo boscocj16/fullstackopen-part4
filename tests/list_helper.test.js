fd9a03e41f66647195f7f653b2710880bca205d8
boscocj16 <boscojoseph79@gmail.com>
2025-06-12 23:40:49 +0530

    4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,9 +1,60 @@
-const { test } = require('node:test')
+const { describe, test } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+describe('total likes', () => {
+  const emptyList = []
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithMultipleBlogs = [
+    {
+      _id: '1',
+      title: 'First Blog',
+      author: 'Author A',
+      url: 'http://a.com',
+      likes: 3,
+      __v: 0
+    },
+    {
+      _id: '2',
+      title: 'Second Blog',
+      author: 'Author B',
+      url: 'http://b.com',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '3',
+      title: 'Third Blog',
+      author: 'Author C',
+      url: 'http://c.com',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(emptyList)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs)
+    assert.strictEqual(result, 15)
+  })
 })
