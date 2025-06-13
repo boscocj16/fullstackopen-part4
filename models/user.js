c049e92c8291d4b9f20f824e7b21bccbafb7a7de
boscocj16 <boscojoseph79@gmail.com>
2025-06-13 15:44:40 +0530

    4.15

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  name: String,
+  passwordHash: { type: String, required: true },
+})
+
+userSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString()
+    delete ret._id
+    delete ret.__v
+    delete ret.passwordHash
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
