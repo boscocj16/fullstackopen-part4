1913970b7e7e3f2ecd91b85ebdb7b99a8aa9678b
boscocj16 <boscojoseph79@gmail.com>
2025-06-13 15:06:12 +0530

    4.9

--- tests/blog_api.test.cjs
+++ tests/blog_api.test.cjs
@@ -7,7 +7,20 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-const initialBlogs = [ /* your blogs here */ ]
+const initialBlogs = [
+  {
+    title: 'First blog',
+    author: 'Author One',
+    url: 'http://first.com',
+    likes: 10,
+  },
+  {
+    title: 'Second blog',
+    author: 'Author Two',
+    url: 'http://second.com',
+    likes: 20,
+  }
+]
 
 beforeAll(async () => {
   await mongoose.connect(process.env.TEST_MONGODB_URI)
@@ -23,6 +36,14 @@ test('blogs are returned as json', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('blog posts have id field defined, not _id', async () => {
+  const response = await api.get('/api/blogs')
+
+  const blog = response.body[0]
+  expect(blog.id).toBeDefined()
+  expect(blog._id).toBeUndefined()
 })
 
 afterAll(async () => {
